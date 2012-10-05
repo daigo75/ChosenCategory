20b76e0d2ef6927a080a111fb039ecfc10eb1a82
Kasper Isager <kasperisager@gmail.com>
2012-10-05 10:20:03 +0200

    Use __DIR__ in URLs

--- default.php
+++ default.php
@@ -1,20 +1,20 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php if (!defined("APPLICATION")) exit();
 
-$PluginInfo['ChosenCategory'] = array(
-   'Name' => 'Chosen Category',
-   'Description' => 'Utilizes the Chosen plugin by <a href="http://www.getharvest.com/">Harvest</a> for super sweet category selections. ',
-   'Version' => '0.9',
-   'Author' => "Kasper K. Isager",
-   'AuthorEmail' => 'kasperisager@gmail.com',
-   'AuthorUrl' => 'http://github.com/kasperisager'
+$PluginInfo["ChosenCategory"] = array(
+   "Name" => "Chosen Category",
+   "Description" => "Utilizes the Chosen plugin by <a href='http://www.getharvest.com/'>Harvest</a> for super sweet category selections.",
+   "Version" => "1.0",
+   "Author" => "Kasper K. Isager",
+   "AuthorEmail" => "kasperisager@gmail.com",
+   "AuthorUrl" => "http://github.com/kasperisager"
 );
 
 class ChosenPlugin extends Gdn_Plugin {
 
 	public function PostController_Render_Before($Sender) {
-		$Sender->AddCSSFile('plugins/ChosenCategory/chosen.css');
-		$Sender->AddJsFile('plugins/ChosenCategory/jquery.chosen.js');
-		$Sender->AddJsFile('plugins/ChosenCategory/plugin.chosen.js');
+		$Sender->AddCSSFile(__DIR__."/chosen.css");
+		$Sender->AddJsFile(__DIR__."/jquery.chosen.js");
+		$Sender->AddJsFile(__DIR__."/plugin.chosen.js");
 	}
 	public function Setup(){}
 
